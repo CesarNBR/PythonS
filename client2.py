4a59a9feac35a1735aa89cc0f166d185b71804e1
CesarNBR <buendia732@gmail.com>
2022-04-12 20:17:26 -0500

    SerPython

---
+++ client2.py
@@ -0,0 +1,19 @@
+import socketio
+
+sio = socketio.Client()
+
+def send_sensor_readings():
+    while True:
+        sio.emit('my_message', {'temp':30})
+        sio.sleep(5) 
+
+@sio.event
+def connect():
+    print('connection established')
+    sio.start_background_task(send_sensor_readings)
+
+@sio.event 
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:5000')
